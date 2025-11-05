f0ce152d44ccc131f8cd691e8056f4c49d0cd7c9
noazlee <noazlee5@gmail.com>
2025-11-04 21:09:58 -0600

    one more uncomment

--- test.py
+++ test.py
@@ -216,7 +216,7 @@ if __name__ == "__main__":
     
     # Save sampled questions to CSV
 
-    # timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     csv_filename = f'outputs/sampled_questions_eedi3_active_{timestamp}.csv'
     save_questions_to_csv(sampled_data, csv_filename)
     print(f"Sampled questions saved to {csv_filename}")
